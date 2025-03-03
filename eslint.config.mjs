2f27f90d628122b8e801e5dabccae182fb661052
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-03-02 21:26:19 -0700

    ESLINT: Update config file to include rules for unused variables.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,22 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
