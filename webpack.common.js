664a05cb1a0e530d600cefe5fd4a838830b4fd90
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-03-02 21:06:12 -0700

    PRETTIER: Run format on prettier.

--- webpack.common.js
+++ webpack.common.js
@@ -1,29 +1,29 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-    target: 'node',
-    entry: './src/extension.ts',
-    resolve: {
-        mainFields: ['browser', 'module', 'main'],
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    externals: {
-        vscode: 'commonjs vscode'
-    },
-    devtool: 'source-map',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'extension.js',
-        libraryTarget: 'commonjs2',
-    }
-}
+  target: "node",
+  entry: "./src/extension.ts",
+  resolve: {
+    mainFields: ["browser", "module", "main"],
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: {
+    vscode: "commonjs vscode",
+  },
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+};
